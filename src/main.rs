2df684797f8eef147531aa5e19702c441180bf65
Simon Renblad <srenblad@m-labs.hk>
2025-02-12 18:02:34 +0800

    add inorder traversal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{collections::VecDeque, fmt::Display, io::Write, fs::File};
+use std::{collections::VecDeque, fmt::{Display, Debug}, io::Write, fs::File};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node<T>{
     val: T,
     left: Option<Box<Node<T>>>,
@@ -15,6 +15,26 @@ impl<T> Node<T> {
             right,
         }
     }
+}
+
+fn inorder<T: Copy + Debug>(root: Box<Node<T>>) -> Vec<T> {
+    let mut out: Vec<T> = Vec::new();
+    let mut stack: Vec<Box<Node<T>>> = Vec::new();
+    let mut current = Some(root);
+    loop {
+        if let Some(val) = current {
+            stack.push(val.clone());
+            current = val.left;
+        } else {
+            if let Some(val) = stack.pop() {
+                out.push(val.val);
+                current = val.right;
+            } else {
+                break;
+            }
+        }
+    }
+    out
 }
 
 fn breadth_first_traversal<T: Write, A: Display>(root: Box<Node<A>>, writer: &mut T) -> std::io::Result<()> {
@@ -71,12 +91,14 @@ fn write_to_dot<T: Write, A: Display>(writer: &mut T, root: Box<Node<A>>) -> std
 }
 
 fn main() {
-    let mut f = File::create_new("graph.dot").unwrap();
+    // let mut f = File::create_new("graph.dot").unwrap();
     let p = Node::new(2,
         Some(Box::new(Node::new(4,
                 None, Some(Box::new(Node::new(5, None, None)))
               ))), None);
-    write_to_dot(&mut f, Box::new(p)).unwrap();
+    // write_to_dot(&mut f, Box::new(p)).unwrap();
+    let a = inorder(Box::new(p));
+    println!("{:?}", a);
 }
 
 //
